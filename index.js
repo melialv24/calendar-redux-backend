24f16cd7b0886dd5556e753eda6084370246abf7
Melissa <malvarez@datagas.com.co>
2022-09-05 17:49:47 -0500

    configurando epress y variables de entorno

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hola Mundo!!!");
+const express = require("express");
+require("dotenv").config();
+
+// crear el servidor de express
+const app = express();
+
+// Directorio público
+// hacer que cuando la persona entre al slash mostrarle el directorio publico
+// el use en express es un middleware, es decir una función que se ejecuta siempre que pase por algún lugar
+app.use(express.static("public"));
+
+//Rutas
+// aquí le digo al cliente cuando alguien
+//solicite un slash que es lo que quiero responder
+/*app.get("/", (req, res) => {
+  //para retornar un objeto json
+  res.json({
+    ok: true,
+  });
+});
+*/
+// Escuchar ,(le voy a decir a mi servidor de express
+// donde escuchar las peticiones) (puerto donde va a correr,
+//un callback que se ejecuta cuando el servidor este arriba )
+app.listen(process.env.PORT, () => {
+  console.log("Melissa hermosa");
+});
